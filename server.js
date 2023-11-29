3e328d93c6362b6feacc7c86869ce7f2d676fde3
Sebastian Radu <sebastainradu0102@gmail.com>
2023-11-29 18:46:53 +0200

    updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 5500;
 
-// Middleware pentru a permite parsarea datelor JSON
 app.use(express.json());
 
 // Ruta GET pentru a returna un mesaj simplu
@@ -10,14 +9,11 @@ app.get('/', (req, res) => {
   res.send('Serviciu RESTful funcțional în Node.js');
 });
 
-// Ruta POST pentru a primi și returna date JSON
 app.post('/api/data', (req, res) => {
   const data = req.body;
-  // Aici poți procesa și returna datele după nevoie
   res.json(data);
 });
 
-// Pornirea serverului
 app.listen(port, () => {
   console.log(`Serverul ascultă la http://localhost:${port}`);
 });
